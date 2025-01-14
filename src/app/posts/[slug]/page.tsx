5f5b36a16b11586330101968554ec87603f52afc
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 17:55:16 +0200

    vercel-third

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,35 +1,12 @@
 import prisma from "@/app/lib/db";
 import { notFound } from "next/navigation";
-import { Metadata } from "next";
 
-// Use Next.js built-in types
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-    return {
-        title: `Post - ${params.slug}`,
-    };
-}
 
-export async function generateStaticParams() {
-    const posts = await prisma.post.findMany({
-        select: {
-            slug: true,
-        },
-    });
-
-    return posts.map((post) => ({
-        slug: post.slug,
-    }));
-}
-
-export default async function PostPage({
-    params,
-}: {
-    params: { slug: string };
-}) {
+export default async function PostPage({ params }: { params: { slug: string } }) {
     const post = await prisma.post.findUnique({
         where: {
             slug: params.slug,
-        },
+        }
     });
 
     if (!post) {
