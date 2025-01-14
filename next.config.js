a2d9dd962b8fdc70613e7a85e588bbb70357321b
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 19:02:49 +0200

    new-first

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enables React's Strict Mode
+  swcMinify: true, // Enables the SWC compiler for faster builds
+  // Add any other configuration options here
+};
+
+export default nextConfig;
