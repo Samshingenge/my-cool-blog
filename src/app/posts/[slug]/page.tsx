6193e156a303e0e427bd15bdd5d9b6c2a2c77aca
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 17:36:37 +0200

    vercel-commit

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,26 @@
 import prisma from "@/app/lib/db";
 import { notFound } from "next/navigation";
 
-interface PageProps {
+type Props = {
   params: {
     slug: string;
   };
+};
+
+// Add 'generateStaticParams' for static generation (optional but recommended)
+export async function generateStaticParams() {
+  const posts = await prisma.post.findMany({
+    select: {
+      slug: true,
+    },
+  });
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 }
 
-export default async function PostPage({ params }: PageProps) {
+export default async function PostPage({ params }: Props) {
     const post = await prisma.post.findUnique({
         where: {
             slug: params.slug,
