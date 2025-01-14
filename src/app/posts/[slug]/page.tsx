2d0f4693267c706a52b2e7619e99497ac6ea903e
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 16:28:33 +0200

    forth-commit

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -4,13 +4,18 @@ import prisma from "@/app/lib/db";
 
 
 
-export default async function PostPage({ params }) {
+export default async function PostPage({ params }: { params: { slug: string } }) {
 
     const post = await prisma.post.findUnique({
         where: {
             slug: params.slug,
         }
     });
+
+    // Handle the case where the post is not found
+    if (!post) {
+        return <div>Post not found</div>; // You can customize this as needed
+    }
 
     return(
         <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
