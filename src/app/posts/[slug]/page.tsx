79d0953f6149037d5fd5d11d2e7705275d24bc1c
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 16:34:44 +0200

    fifth-commit

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -19,8 +19,8 @@ export default async function PostPage({ params }: { params: { slug: string } })
 
     return(
         <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-            <h1 className="text-3xl font-semibold">{post?.title}</h1>
-            <p>{post?.content}</p>
+            <h1 className="text-3xl font-semibold">{post.title}</h1>
+            <p>{post.content}</p>
         </main>
     )
     
