39a5c32ef4ea567b147965b65189ae9bb2a9e01f
Samshingenge <1samnatangwe@gmail.com>
2025-01-14 16:50:18 +0200

    seventh-commit

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,12 +1,10 @@
 import prisma from "@/app/lib/db";
 
-interface PostPageProps {
-    params: {
-        slug: string;
-    };
-}
-
-export default async function PostPage({ params }: PostPageProps) {
+export default async function PostPage({
+    params,
+}: {
+    params: { slug: string };
+}) {
     const post = await prisma.post.findUnique({
         where: {
             slug: params.slug,
@@ -15,13 +13,13 @@ export default async function PostPage({ params }: PostPageProps) {
 
     // Handle the case where the post is not found
     if (!post) {
-        return <div>Post not found</div>; // You can customize this as needed
+        return <div>Post not found</div>;
     }
 
     return (
         <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
-            <h1 className="text-3xl font-semibold">{post.title}</h1>
-            <p>{post.content}</p>
+            <h1 className="text-3xl font-semibold">{post?.title}</h1>
+            <p>{post?.content}</p>
         </main>
     );
 }
